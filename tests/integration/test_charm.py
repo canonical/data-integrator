45b8b68b2bbc86695d80f8ff228cc497b2b1ad59
Ubuntu <ubuntu@dev2.project.serverstack>
2022-12-07 20:24:07 +0000

    Add first version of integration tests

---
+++ tests/integration/test_charm.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+
+import psycopg2
+import pytest
+from pymongo import MongoClient
+from pytest_operator.plugin import OpsTest
+
+from tests.integration.connector import MysqlConnector
+from tests.integration.constants import (
+    DATA_INTEGRATOR,
+    DATABASE_NAME,
+    MONGODB,
+    MYSQL,
+    POSTGRESQL,
+)
+from tests.integration.helpers import (
+    build_postgresql_connection_string,
+    create_table_mysql,
+    fetch_action_get_credentials,
+    insert_data_mysql,
+    read_data_mysql,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    data_integrator_charm = await ops_test.build_charm(".")
+    await asyncio.gather(
+        ops_test.model.deploy(
+            data_integrator_charm, application_name="data-integrator", num_units=1, series="jammy"
+        )
+    )
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR])
+    assert ops_test.model.applications[DATA_INTEGRATOR].status == "blocked"
+
+    # config database name
+
+    config = {"database": DATABASE_NAME}
+    await ops_test.model.applications[DATA_INTEGRATOR].set_config(config)
+
+    # test the active/waiting status for relation
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR])
+    assert ops_test.model.applications[DATA_INTEGRATOR].status == "active"
+
+
+@pytest.mark.skip  # skipping as we can't deploy MYSQL (https://github.com/canonical/mysql-operator/pull/73)
+async def test_deploy_and_relate_mysql(ops_test: OpsTest):
+    """Test the relation with MySQL and database accessibility."""
+    await asyncio.gather(
+        ops_test.model.deploy(
+            "mysql", channel="edge", application_name=MYSQL, num_units=1, series="focal"
+        )
+    )
+    await ops_test.model.wait_for_idle(apps=[MYSQL])
+    assert ops_test.model.applications[MYSQL].status == "active"
+    await ops_test.model.add_relation(DATA_INTEGRATOR, MYSQL)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, MYSQL])
+    assert ops_test.model.applications[DATA_INTEGRATOR].status == "active"
+
+    # get credential for MYSQL
+    credentials = fetch_action_get_credentials(ops_test.model.applications[DATA_INTEGRATOR][0])
+
+    # test connection for MYSQL with retrieved credentials
+    # connection configuration
+    config = {
+        "user": credentials[MYSQL]["username"],
+        "password": credentials[MYSQL]["password"],
+        "host": credentials[MYSQL]["endpoints"].split(":")[0],
+        "database": DATABASE_NAME,
+        "raise_on_warnings": False,
+    }
+
+    with MysqlConnector(config) as cursor:
+
+        create_table_mysql(cursor, DATABASE_NAME)
+
+        insert_data_mysql(
+            cursor,
+            DATABASE_NAME,
+            credentials[MYSQL]["username"],
+            credentials[MYSQL]["password"],
+            credentials[MYSQL]["endpoints"],
+            credentials[MYSQL]["version"],
+            credentials[MYSQL]["read-only-endpoints"],
+        )
+
+        rows = read_data_mysql(cursor, credentials[MYSQL]["username"])
+        first_row = rows[0]
+        # username, password, endpoints, version, ro-endpoints
+        assert first_row[1] == credentials[MYSQL]["username"]
+        assert first_row[2] == credentials[MYSQL]["password"]
+        assert first_row[3] == credentials[MYSQL]["endpoints"]
+        assert first_row[4] == credentials[MYSQL]["version"]
+        assert first_row[5] == credentials[MYSQL]["read-only-endpoints"]
+
+    #  remove relation and test connection again
+    await ops_test.model.applications[DATA_INTEGRATOR].remove_relation(
+        f"{DATA_INTEGRATOR}:mysql", f"{MYSQL}:database"
+    )
+
+    await ops_test.model.wait_for_idle(apps=[MYSQL, DATA_INTEGRATOR])
+    await ops_test.model.add_relation(DATA_INTEGRATOR, MYSQL)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, MYSQL])
+
+    # join with another relation and check the accessibility of the previously created database
+    new_credentials = await fetch_action_get_credentials(
+        ops_test.model.applications[DATA_INTEGRATOR].units[0]
+    )
+
+    new_config = {
+        "user": new_credentials[MYSQL]["username"],
+        "password": new_credentials[MYSQL]["password"],
+        "host": new_credentials[MYSQL]["endpoints"].split(":")[0],
+        "database": DATABASE_NAME,
+        "raise_on_warnings": False,
+    }
+
+    with MysqlConnector(new_config) as cursor:
+        rows = read_data_mysql(cursor, credentials[MYSQL]["username"])
+        first_row = rows[0]
+        # username, password, endpoints, version, ro-endpoints
+        assert first_row[1] == credentials[MYSQL]["username"]
+        assert first_row[2] == credentials[MYSQL]["password"]
+        assert first_row[3] == credentials[MYSQL]["endpoints"]
+        assert first_row[4] == credentials[MYSQL]["version"]
+        assert first_row[5] == credentials[MYSQL]["read-only-endpoints"]
+
+
+@pytest.mark.skip  # skipping as we can't reconnect to same database (https://github.com/canonical/postgresql-k8s-operator/issues/64)
+async def test_deploy_and_relate_postgresql(ops_test: OpsTest):
+    """Test the relation with PostgreSQL and database accessibility."""
+    await asyncio.gather(
+        ops_test.model.deploy(
+            "postgresql", channel="edge", application_name=POSTGRESQL, num_units=1, series="focal"
+        )
+    )
+    await ops_test.model.wait_for_idle(apps=[POSTGRESQL])
+    assert ops_test.model.applications[POSTGRESQL].status == "active"
+    await ops_test.model.add_relation(DATA_INTEGRATOR, POSTGRESQL)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, POSTGRESQL])
+    assert ops_test.model.applications[DATA_INTEGRATOR].status == "active"
+
+    # get credential for PostgreSQL
+    credentials = await fetch_action_get_credentials(
+        ops_test.model.applications[DATA_INTEGRATOR].units[0]
+    )
+    # Build the complete connection string to connect to the database.
+    connection_string = build_postgresql_connection_string(credentials)
+    version = credentials[POSTGRESQL]["version"]
+
+    # test connection for PostgreSQL with retrieved credentials
+    with psycopg2.connect(connection_string) as connection, connection.cursor() as cursor:
+        # Check that it's possible to write and read data from the database that
+        # was created for the application.
+        connection.autocommit = True
+        cursor.execute(f"DROP TABLE IF EXISTS {DATABASE_NAME};")
+        cursor.execute(f"CREATE TABLE {DATABASE_NAME}(data TEXT);")
+        cursor.execute(f"INSERT INTO {DATABASE_NAME}(data) VALUES('some data');")
+        cursor.execute(f"SELECT data FROM {DATABASE_NAME};")
+        data = cursor.fetchone()
+        assert data[0] == "some data"
+
+        # Check the version that the application received is the same on the database server.
+        cursor.execute("SELECT version();")
+        data = cursor.fetchone()[0].split(" ")[1]
+
+        assert version == data
+
+    #  remove relation and test connection again
+    await ops_test.model.applications[DATA_INTEGRATOR].remove_relation(
+        f"{DATA_INTEGRATOR}:postgresql", f"{POSTGRESQL}:database"
+    )
+
+    await ops_test.model.wait_for_idle(apps=[POSTGRESQL, DATA_INTEGRATOR])
+    await ops_test.model.add_relation(DATA_INTEGRATOR, POSTGRESQL)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, POSTGRESQL])
+
+    new_credentials = await fetch_action_get_credentials(
+        ops_test.model.applications[DATA_INTEGRATOR].units[0]
+    )
+
+    # Build the complete connection string to connect to the database.
+    new_connection_string = build_postgresql_connection_string(new_credentials)
+    # check that new credentials are provided
+    assert new_connection_string != connection_string
+
+    # Connect to the database using different credentials.
+    with psycopg2.connect(new_connection_string) as connection, connection.cursor() as cursor:
+        # Read data from previously created database.
+        cursor.execute(f"SELECT data FROM {DATABASE_NAME};")
+        data = cursor.fetchone()
+        assert data[0] == "some data"
+
+
+async def test_deploy_and_relate_mongodb(ops_test: OpsTest):
+    """Test the relation with MongoDB and database accessibility."""
+    await asyncio.gather(
+        ops_test.model.deploy(
+            "mongodb", channel="dpe/edge", application_name=MONGODB, num_units=1, series="focal"
+        )
+    )
+    await ops_test.model.wait_for_idle(apps=[MONGODB])
+    assert ops_test.model.applications[MONGODB].status == "active"
+    await ops_test.model.add_relation(DATA_INTEGRATOR, MONGODB)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, MONGODB])
+    assert ops_test.model.applications[DATA_INTEGRATOR].status == "active"
+
+    # get credential for MongoDB
+    credentials = await fetch_action_get_credentials(
+        ops_test.model.applications[DATA_INTEGRATOR].units[0]
+    )
+
+    connection_string = credentials[MONGODB]["uris"]
+
+    client = MongoClient(
+        connection_string,
+        directConnection=False,
+        connect=False,
+        serverSelectionTimeoutMS=1000,
+        connectTimeoutMS=2000,
+    )
+    # test some operations
+    db = client[DATABASE_NAME]
+    test_collection = db["test_collection"]
+    ubuntu = {"release_name": "Focal Fossa", "version": 20.04, "LTS": True}
+    test_collection.insert_one(ubuntu)
+
+    query = test_collection.find({}, {"release_name": 1})
+    assert query[0]["release_name"] == "Focal Fossa"
+
+    ubuntu_version = {"version": 20.04}
+    ubuntu_name_updated = {"$set": {"release_name": "Fancy Fossa"}}
+    test_collection.update_one(ubuntu_version, ubuntu_name_updated)
+
+    query = test_collection.find({}, {"release_name": 1})
+    assert query[0]["release_name"] == "Fancy Fossa"
+
+    client.close()
+
+    # drop relation and get new credential for the same collection
+    await ops_test.model.applications[DATA_INTEGRATOR].remove_relation(
+        f"{DATA_INTEGRATOR}:mongodb", f"{MONGODB}:database"
+    )
+
+    await ops_test.model.wait_for_idle(apps=[MONGODB, DATA_INTEGRATOR])
+    await ops_test.model.add_relation(DATA_INTEGRATOR, MONGODB)
+    await ops_test.model.wait_for_idle(apps=[DATA_INTEGRATOR, MONGODB])
+
+    new_credentials = await fetch_action_get_credentials(
+        ops_test.model.applications[DATA_INTEGRATOR].units[0]
+    )
+    new_connection_string = new_credentials[MONGODB]["uris"]
+
+    # test that different credentials are provided
+    assert connection_string != new_connection_string
+    client = MongoClient(
+        new_connection_string,
+        directConnection=False,
+        connect=False,
+        serverSelectionTimeoutMS=1000,
+        connectTimeoutMS=2000,
+    )
+    # test collection accessibility
+    db = client[DATABASE_NAME]
+    test_collection = db["test_collection"]
+    query = test_collection.find({}, {"release_name": 1})
+    assert query[0]["release_name"] == "Fancy Fossa"
+
+    client.close()
