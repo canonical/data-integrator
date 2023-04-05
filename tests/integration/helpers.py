40da5883859fa3d28421d7742bd99faf16122210
welpaolo <paolo.sottovia@canonical.com>
2023-04-05 12:52:20 +0200

    Fix kafka test after snap restructure (#25)
    Fix kafka test

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -88,14 +88,15 @@ def check_logs(model_full_name: str, kafka_unit_name: str, topic: str) -> None:
         AssertionError: if logs aren't found for desired topic
     """
     log_directory = (
-        "/var/snap/charmed-kafka/common/log-data"
+        "/var/snap/charmed-kafka/common/var/lib/kafka/data"
         if "k8s" not in kafka_unit_name
         else "/var/lib/juju/storage/log-data"
     )
 
     container = "--container kafka " if "k8s" in kafka_unit_name else ""
+    sudo = "sudo -i " if "k8s" not in kafka_unit_name else ""
     logs = check_output(
-        f"JUJU_MODEL={model_full_name} juju ssh {container} {kafka_unit_name} 'find {log_directory}'",
+        f"JUJU_MODEL={model_full_name} juju ssh {container} {kafka_unit_name} {sudo} 'find {log_directory}'",
         stderr=PIPE,
         shell=True,
         universal_newlines=True,
