71de27120c8edbc3039e11c3412fbf2bd2a937a8
Dragomir Penev <dragomir.penev@canonical.com>
2026-01-16 13:28:53 +0200

    Factor out microceph plugin and reduce loop device size

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,13 +2,18 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 import logging
+import os
 import socket
+import subprocess
+import time
 import uuid
 from pathlib import Path
 
+import boto3
+import botocore
 import pytest
-import pytest_microceph
 from pytest_operator.plugin import OpsTest
 from spark_test.core.s3 import Credentials
 
@@ -16,6 +21,7 @@ TEST_BUCKET_NAME = "kyuubi-test"
 TEST_PATH_NAME = "spark-events/"
 HOST_IP = socket.gethostbyname(socket.gethostname())
 
+_BUCKET = "testbucket"
 
 logger = logging.getLogger(__name__)
 
@@ -61,8 +67,49 @@ def bucket_name():
 
 
 @pytest.fixture(scope="module")
-def credentials(microceph: pytest_microceph.ConnectionInformation):
+def credentials():
     """S3 credentials to connect to S3 storage."""
-    yield Credentials(
-        access_key=microceph.access_key_id, secret_key=microceph.secret_access_key, host=HOST_IP
-    )
+    if not os.environ.get("CI") == "true":
+        raise Exception("Not running on CI. Skipping microceph installation")
+    logger.info("Setting up microceph")
+    subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
+    subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
+    subprocess.run(["sudo", "microceph", "disk", "add", "loop,1G,3"], check=True)
+    subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
+    output = subprocess.run(
+        [
+            "sudo",
+            "microceph.radosgw-admin",
+            "user",
+            "create",
+            "--uid",
+            "test",
+            "--display-name",
+            "test",
+        ],
+        capture_output=True,
+        check=True,
+        encoding="utf-8",
+    ).stdout
+    key = json.loads(output)["keys"][0]
+    key_id = key["access_key"]
+    secret_key = key["secret_key"]
+    logger.info("Creating microceph bucket")
+    for attempt in range(3):
+        try:
+            boto3.client(
+                "s3",
+                endpoint_url="http://localhost",
+                aws_access_key_id=key_id,
+                aws_secret_access_key=secret_key,
+            ).create_bucket(Bucket=_BUCKET)
+        except botocore.exceptions.EndpointConnectionError:
+            if attempt == 2:
+                raise
+            # microceph is not ready yet
+            logger.info("Unable to connect to microceph via S3. Retrying")
+            time.sleep(1)
+        else:
+            break
+    logger.info("Set up microceph")
+    yield Credentials(access_key=key_id, secret_key=secret_key, host=HOST_IP)
