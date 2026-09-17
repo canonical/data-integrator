6feb9d62a641fd1f41f0bc2cb2b60d91f4a6d815
MattiaSarti <nmattia1@gmail.com>
2026-09-17 13:49:29 +0200

    test: cross-test from mlflow's pull request

--- tests/integration/test_mlflow.py
+++ tests/integration/test_mlflow.py
@@ -77,7 +77,10 @@ async def test_deploy_and_relate_mlflow(ops_test: OpsTest, cloud_name: str):
         ops_test.model.deploy(
             POSTGRESQL_K8S, channel="14/stable", config={"profile": "testing"}, trust=True
         ),
-        ops_test.model.deploy(MLFLOW, channel="latest/edge", trust=True),
+        # TODO: restore once https://github.com/canonical/mlflow-operator/pull/494 lands on main,
+        # that is on channel "latest/edge", as python-libjuju breaks with this channel format:
+        ops_test.juju("deploy", MLFLOW, "--channel", "latest/edge/pr-490", "--trust"),
+        # ops_test.model.deploy(MLFLOW, channel="latest/edge", trust=True),
     )
     await ops_test.model.wait_for_idle(
         apps=[MINIO, POSTGRESQL_K8S], status="active", raise_on_blocked=False, timeout=1000
